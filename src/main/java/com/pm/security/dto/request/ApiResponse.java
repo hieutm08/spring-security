ed0b2fab646d38817a988035ad2f81fd35e1e22f
hieutm <hieutm0805@gamil.com>
2025-04-21 15:26:54 +0900

    init source code

---
+++ src/main/java/com/pm/security/dto/request/ApiResponse.java
@@ -0,0 +1,17 @@
+package com.pm.security.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    int code = 202;
+    String message;
+    T result;
+}
---
+++ src/main/java/com/pm/security/dto/request/UserUpdateRequest.java
@@ -0,0 +1,18 @@
+package com.pm.security.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserUpdateRequest {
+     String password;
+     String firstName;
+     String lastName;
+     LocalDate dateOfBirth;
+}
